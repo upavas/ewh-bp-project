159774c229327de24a56952d55bde3db34983b8c
cccommun <cccommun@gmail.com>
2011-01-29 15:48:45 +0000

    1st draft Help page working and able to switch to a garbage measure page (just buttons - no function) Bug : bottom of pages not accessible when phone tilted

--- trunk/bp-app/src/com/example/android/home/Measure.java
+++ trunk/bp-app/src/com/example/android/home/Measure.java
@@ -1,23 +1,25 @@
 package com.example.android.home;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Home extends Activity {
+
+public class Measure extends Activity {
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-        Button next = (Button) findViewById(R.id.button);
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.measure);
+
+        Button next = (Button) findViewById(R.id.button_help);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Measure.class);
-                startActivityForResult(myIntent, 0);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
             }
 
         });
