29206d8a82b8b3bec76d0e5c25f12bd690b13bd4
cccommun <cccommun@gmail.com>
2011-02-02 16:45:47 +0000

    - Improved display of Measure activity (text, colors) - fake BP acquisition and plotting

--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/Measure.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/Measure.java
@@ -2,17 +2,30 @@
 package com.ewhoxford.android.bloodpressure;
 
 //Import resources
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.ewhoxford.android.bloodpressure.R;
 
 //Import Android stuff
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 //Class Measure : activity that pops when the user wants to start taking blood pressure
 public class Measure extends Activity implements OnClickListener
 {
+	int[] values;
+	int valuesEnd;
+	Random rand;
+	int d;
+	GraphView graph;
+	
+	
+	
 	// To be performed on the creation
     public void onCreate(Bundle savedInstanceState)
     {
@@ -30,7 +43,44 @@ public class Measure extends Activity implements OnClickListener
 
 		// #### End of Set up click listeners for all the buttons
 
+		// Simulate fake BP acquisition
+		values = new int[60];
+		rand = new Random();
+		d = 4;
+		graph = (GraphView) findViewById(R.id.graph);
+		
+		values[0] = 40;
+		valuesEnd = 0;
+
+		addValue();
 	}
+    
+    protected void addValue()
+    {
+    	valuesEnd = valuesEnd + 1;
+    	int i = valuesEnd;
+		values[i] = values[i-1] + rand.nextInt(d+1) - d/2;
+		
+		graph.plotValues(values, valuesEnd);
+    	
+		if (i<59)
+		{
+    	
+        final Handler handler = new Handler(); 
+        Timer t = new Timer(); 
+        t.schedule(new TimerTask() { 
+                public void run() { 
+                        handler.post(new Runnable() { 
+                                public void run() { 
+                                	addValue();
+                                } 
+                        }); 
+                } 
+        }, 80); 
+        
+		}
+    }
+    
     
 	// event : click on something
 	public void onClick(View V)
--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/ValuesView.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/ValuesView.java
@@ -7,6 +7,8 @@ import com.ewhoxford.android.bloodpressure.R;
 // Import Android stuff
 import android.content.Context;
 import android.graphics.*;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
 import android.graphics.drawable.*;
 import android.view.*;
 import android.util.AttributeSet;
@@ -18,6 +20,8 @@ public class ValuesView extends View
 	// Attributes
 	private Paint PaintRectLeft;	// Paint object for background rectangle of the left
 	private Paint PaintRectRight;	// Paint object for background rectangle of the right
+	private Paint PaintFontLeft;	// Paint object for fonts of the left rectangle
+	private Paint PaintFontRight;	// Paint object for fonts of the right rectangle
 
 	// Constructor 1/3
 	public ValuesView(Context context)
@@ -67,22 +71,63 @@ public class ValuesView extends View
 		PaintRectRight.setColor(r.getColor(R.color.valuesview_background_color_right));
 		PaintRectRight.setStrokeWidth(1);
 		PaintRectRight.setStyle(Paint.Style.FILL_AND_STROKE);
+		
+		// Paint object for fonts of the left rectangle
+		PaintFontLeft = new Paint(Paint.ANTI_ALIAS_FLAG);
+		PaintFontLeft.setColor(r.getColor(R.color.valuesview_foreground_color_left));
+		PaintFontLeft.setStyle(Style.FILL);
+		PaintFontLeft.setTextAlign(Paint.Align.LEFT);
+
+		// Paint object for fonts of the left rectangle
+		PaintFontRight = new Paint(Paint.ANTI_ALIAS_FLAG);
+		PaintFontRight.setColor(r.getColor(R.color.valuesview_foreground_color_right));
+		PaintFontRight.setStyle(Style.FILL);
+		PaintFontRight.setTextAlign(Paint.Align.RIGHT);
 
 	}
 
 	// Draw method
 	protected void onDraw(Canvas canvas)
 	{
+		// Temporary variables
+		FontMetrics fm;
+		float y;
+		
 		// Measures
 		int w = getMeasuredWidth();
 		int h = getMeasuredHeight();
 		
 		// Draw the background rectangle on the left
-		canvas.drawRect(0, 0, (float) 0.33 * w, h, PaintRectLeft);
+		canvas.drawRect(0, 0, 0.33f * w, h, PaintRectLeft);
 
 		// Draw the background rectangle on the right
-		canvas.drawRect((float) 0.33 * w, 0, w, h, PaintRectRight);
+		canvas.drawRect(0.33f * w, 0, w, h, PaintRectRight);
+		
+		// Draw the text in the left rectangle
+		PaintFontLeft.setTextSize(h/3 * 0.75f);
+		//PaintFontLeft.setTextScaleX(w / h);
+		
+		// Centering in Y: measure ascent/descent first
+		fm = PaintFontLeft.getFontMetrics();
+		y = h / 6 - (fm.ascent + fm.descent) / 2;
+
+		canvas.drawText("DIA", 5, y, PaintFontLeft);
+		canvas.drawText("SYS", 5, h/3 + y, PaintFontLeft);
+		canvas.drawText("PUL", 5, 2*h/3 + y, PaintFontLeft);
+
+		// Draw the text in the right rectangle
+		PaintFontRight.setTextSize(h/3 * 0.75f);
+		//PaintFontLeft.setTextScaleX(w / h);
+		
+		// Centering in Y: measure ascent/descent first
+		fm = PaintFontRight.getFontMetrics();
+		y = h / 6 - (fm.ascent + fm.descent) / 2;
+
+		canvas.drawText("0 mmHg", w-5, y, PaintFontRight);
+		canvas.drawText("0 mmHg", w-5, h/3 + y, PaintFontRight);
+		canvas.drawText("0 bpm    ", w-5, 2*h/3 + y, PaintFontRight);
 		
 	}
+
 
 }
