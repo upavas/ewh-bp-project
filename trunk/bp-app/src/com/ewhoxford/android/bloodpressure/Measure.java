efe7a461929de277067171d803561cfdbbcc4142
mauro dsantos <mauro.dsantos@gmail.com>
2011-03-13 01:36:53 +0000

    integrated xyplot graph in measure page

--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/Measure.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/Measure.java
@@ -5,7 +5,16 @@ package com.ewhoxford.android.bloodpressure;
 import java.io.File;
 import java.io.FileInputStream;
 
+import com.androidplot.ui.layout.AnchorPosition;
+import com.androidplot.ui.layout.XLayoutStyle;
+import com.androidplot.ui.layout.YLayoutStyle;
+import com.androidplot.ui.widget.Widget;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.LineAndPointRenderer;
+import com.androidplot.xy.XYPlot;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -21,6 +30,7 @@ public class Measure extends Activity implements OnClickListener {
 	GraphView graph;
 	BPSignalProcessing signalProcessing;
 	boolean saveFile = false;
+	private XYPlot mySimpleXYPlot;
 
 	// To be performed on the creation
 	public void onCreate(Bundle savedInstanceState) {
@@ -64,9 +74,34 @@ public class Measure extends Activity implements OnClickListener {
 
 		// #### End of Set up click listeners for all the buttons
 
-		graph = (GraphView) findViewById(R.id.graph);
+		//graph = (GraphView) findViewById(R.id.graph);
 
-		graph.invalidate();
+		//graph.invalidate();
+		
+		// initialize our XYPlot reference:
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+		ReadCSV r = new ReadCSV();
+		int[][] vals1 = r.readCSV();
+        // add a new series
+        mySimpleXYPlot.addSeries(new SimpleXYSeries(vals1.length,vals1), LineAndPointRenderer.class, new LineAndPointFormatter(Color.rgb(200, 0, 0), Color.rgb(0, 0, 200)));
+
+        // reduce the number of range labels
+        mySimpleXYPlot.getGraphWidget().setRangeTicksPerLabel(4);
+
+        // reposition the domain label to look a little cleaner:
+        Widget domainLabelWidget = mySimpleXYPlot.getDomainLabelWidget();
+
+        mySimpleXYPlot.position(domainLabelWidget,                     // the widget to position
+                                45,                                    // x position value, in this case 45 pixels
+                                XLayoutStyle.ABSOLUTE_FROM_LEFT,       // how the x position value is applied, in this case from the left
+                                0,                                     // y position value
+                                YLayoutStyle.ABSOLUTE_FROM_BOTTOM,     // how the y position is applied, in this case from the bottom
+                                AnchorPosition.LEFT_BOTTOM);           // point to use as the origin of the widget being positioned
+
+        // get rid of the visual aids for positioning:
+        mySimpleXYPlot.disableAllMarkup();
+				
+		
 		acquireDataFromMouse();
 	}
 
