31b2a7b0ea5ba52f0bf1f22b73d691783231abb9
mauro dsantos <mauro.dsantos@gmail.com>
2011-03-28 18:45:57 +0100

    committing last changes

--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/signalProcessing/GetDecrCurve.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/signalProcessing/GetDecrCurve.java
@@ -2,105 +2,105 @@ package com.ewhoxford.android.bloodpressure.signalProcessing;
 
 /**
  * 
- * @author user
- * Get decreasing part of the signal
+ * @author user Get decreasing part of the signal
  */
 public class GetDecrCurve {
-	
+
 	/**
 	 * 
 	 * @param signal
-	 * @return array with curve correspondent to the decreasing part of the signal
+	 * @return array with curve correspondent to the decreasing part of the
+	 *         signal
+	 * @throws ArrayIsNullException
 	 * @see curve
 	 */
-	public TimeSeriesMod getDecrCurve(TimeSeriesMod signal){
-		
+	public TimeSeriesMod getDecrCurve(TimeSeriesMod signal) {
+
 		// get max and index values of the original signal
 		MaxResult maxval = ArrayOperator.maxValue(signal.pressure);
-		
+
 		// initialize variables
 		TimeSeriesMod decSignal = new TimeSeriesMod();
-		int l = signal.pressure.length-maxval.index;
+		int l = signal.pressure.length - maxval.index;
 		double[] curve = new double[l];
 		float[] time = new float[l];
-		
+
 		// apply additional filter - remove transitions/jumps higher than 3 mmHg
 		curve[0] = signal.pressure[maxval.index];
 		time[0] = signal.time[maxval.index];
-		for (int i = 1; i < l; ++i){
+		for (int i = 1; i < l; ++i) {
 			int j = i + maxval.index;
-			if (Math.abs(signal.pressure[j - 1] - signal.pressure[j]) > 3){
+			if (Math.abs(signal.pressure[j - 1] - signal.pressure[j]) > 3) {
 				signal.pressure[j] = signal.pressure[j - 1];
 				curve[i] = signal.pressure[j - 1];
-			}
-			else
+			} else
 				curve[i] = signal.pressure[j];
 			time[i] = signal.time[j];
 		}
-		
+
 		// define window for median filter
 		double[] window = new double[5];
-		
+
 		// APPLY MEDIAN FILTER
-	    for (int i = 2; i < curve.length - 2; ++i){
-	    	
-	        // pick up window elements
-	    	for (int j = 0; j < 5; ++j)
-	    		window[j] = (double) curve[i - 2 + j];
-	    	
-	    	// order elements (only half of them)
-	        for (int j = 0; j < 3; ++j){
-	        	
-	           // find position of minimum element
-	           int min = j;
-	           for (int k = j + 1; k < 5; ++k)
-	              if (window[k] < window[min])
-	                 min = k;
-	           
-	           // put found minimum element in its place
-	           double temp = window[j];
-	           window[j] = window[min];
-	           window[min] = temp;
-	        }
-	        
-	        // get result - the middle element
-	        curve[i - 2] = window[2];
-	    }
-	    
-	    // SELECT VALUES FROM 220 TO 40 mm Hg
-	    // initialize variables
-	    boolean FLAG1 = false;
-	    boolean FLAG2 = false;
-	    int indxup = 0;
-	    int indxdown = 0;
-	    
-	    // get indexes
-	    for (int i = 0; i < curve.length; ++i){
-	    	if (curve[i] <= 220 && FLAG1 == false){
-	    		indxup = i;
-	    		FLAG1 = true;
-	    	}
-	    	if (curve[i] <= 40 && FLAG2 == false){
-	    		indxdown = i;
-	    		FLAG2 = true;
-	    	}	
-	    }
-	    
-	    int l1 = indxdown - indxup + 1;
+		for (int i = 2; i < curve.length - 2; ++i) {
+
+			// pick up window elements
+			for (int j = 0; j < 5; ++j)
+				window[j] = (double) curve[i - 2 + j];
+
+			// order elements (only half of them)
+			for (int j = 0; j < 3; ++j) {
+
+				// find position of minimum element
+				int min = j;
+				for (int k = j + 1; k < 5; ++k)
+					if (window[k] < window[min])
+						min = k;
+
+				// put found minimum element in its place
+				double temp = window[j];
+				window[j] = window[min];
+				window[min] = temp;
+			}
+
+			// get result - the middle element
+			curve[i - 2] = window[2];
+		}
+
+		// SELECT VALUES FROM 220 TO 40 mm Hg
+		// initialize variables
+		boolean FLAG1 = false;
+		boolean FLAG2 = false;
+		int indxup = 0;
+		int indxdown = 0;
+
+		// get indexes
+		for (int i = 0; i < curve.length; ++i) {
+			if (curve[i] <= 220 && FLAG1 == false) {
+				indxup = i;
+				FLAG1 = true;
+			}
+			if (curve[i] <= 40 && FLAG2 == false) {
+				indxdown = i;
+				FLAG2 = true;
+			}
+		}
+
+		int l1 = indxdown - indxup + 1;
 		double[] curvef = new double[l1];
 		float[] timef = new float[l1];
 		int j = 0;
-		
+
 		// select curve correspondent to the indexes 220 to 40
-		for (int i = indxup; i <= indxdown; ++i){
-	    	curvef[j] = curve[i];
-	    	timef[j] = time[i];
-	    	j = j + 1;
-	    }
-		
+		for (int i = indxup; i <= indxdown; ++i) {
+			curvef[j] = curve[i];
+			timef[j] = time[i];
+			j = j + 1;
+		}
+
 		// output
-	    decSignal.setPressure(curvef);
-	    decSignal.setTime(timef);
+		decSignal.setPressure(curvef);
+		decSignal.setTime(timef);
 		return decSignal;
 	}
 }
--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/signalProcessing/PressureValues.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/signalProcessing/PressureValues.java
@@ -1,9 +1,8 @@
 package com.ewhoxford.android.bloodpressure.signalProcessing;
 
 /**
- * 
- * @author user
- * Determines systolic, diastolic and mean arterial blood pressure values
+ * @author user Determines systolic, diastolic and mean arterial blood pressure
+ *         values
  */
 public class PressureValues {
 
@@ -12,9 +11,11 @@ public class PressureValues {
 	 * @param oscillations
 	 * @param curve
 	 * @return SP, DP, MAP, and interpolated signal
+	 * @throws ArrayIsNullException
 	 * @see bloodPressure
 	 */
-	public BloodPressureValues pressureValues(TimeSeriesMod oscillations, TimeSeriesMod curve){
+	public BloodPressureValues pressureValues(TimeSeriesMod oscillations,
+			TimeSeriesMod curve) {
 		// initialize variables
 		BloodPressureValues bloodPressure = new BloodPressureValues();
 		int defaultSize = 30;
@@ -33,20 +34,20 @@ public class PressureValues {
 		boolean lookForMax = true;
 		int jmx = 0;
 		int jmn = 0;
-		
+
 		// detect oscillation peaks (max and min)
-		for (int i = 0; i < oscillations.pressure.length; ++i){
+		for (int i = 0; i < oscillations.pressure.length; ++i) {
 			value = oscillations.pressure[i];
-			if (value > mx){
+			if (value > mx) {
 				mx = value;
 				mxPos = i;
 			}
-			if (value < mn){
+			if (value < mn) {
 				mn = value;
 				mnPos = i;
 			}
-			if (lookForMax==true){
-				if (value < (mx - delta)){
+			if (lookForMax == true) {
+				if (value < (mx - delta)) {
 					maxOscillations[jmx] = mx;
 					maxTime[jmx] = oscillations.time[mxPos];
 					maxTimeInt[jmx] = mxPos;
@@ -55,9 +56,8 @@ public class PressureValues {
 					jmx = jmx + 1;
 					lookForMax = false;
 				}
-			}
-			else{
-				if (value > (mn + delta)){
+			} else {
+				if (value > (mn + delta)) {
 					minOscillations[jmn] = mn;
 					minTime[jmn] = oscillations.time[mnPos];
 					mx = value;
@@ -67,86 +67,86 @@ public class PressureValues {
 				}
 			}
 		}
-		
+
 		// remove extra positions that were not occupied
 		boolean Flag = false;
 		int indx = 0;
-				
-		for (int i = 0; i < maxTime.length; ++i){
-			if (maxTime[i] == 0 && Flag == false){
+
+		for (int i = 0; i < maxTime.length; ++i) {
+			if (maxTime[i] == 0 && Flag == false) {
 				indx = i;
 				Flag = true;
 			}
 		}
-		
+
 		float[] maxTimeMod = new float[indx];
 		int[] maxTimeIntMod = new int[indx];
 		double[] maxOscillationsMod = new double[indx];
-		
-		for (int i = 0; i < indx; ++i){
+
+		for (int i = 0; i < indx; ++i) {
 			maxTimeMod[i] = maxTime[i];
 			maxTimeIntMod[i] = maxTimeInt[i];
 			maxOscillationsMod[i] = maxOscillations[i];
-		} 
-		
+		}
+
 		// GET MEAN ARTERIAL PRESSURE
 		boolean FLAG = false;
 		int MAPPos = 0;
 		double MAP = 0;
-		
-		while (FLAG == false){
+
+		while (FLAG == false) {
 			MaxResult maxMAP = ArrayOperator.maxValue(maxOscillationsMod);
-			if (curve.pressure[maxTimeInt[maxMAP.index]] > 160){
+			if (curve.pressure[maxTimeInt[maxMAP.index]] > 160) {
 				maxOscillationsMod[maxMAP.index] = 0;
-			}
-			else{
+			} else {
 				FLAG = true;
 				MAPPos = maxTimeInt[maxMAP.index];
 				MAP = curve.pressure[maxTimeInt[maxMAP.index]];
 			}
 		}
-		
+
 		// perform linear interpolation to find the other pressure values
-		maxOscillationsInterp = LinearInterpolation.linearInterpolation(maxTimeMod, maxOscillationsMod, oscillations.time);
-		
+		maxOscillationsInterp = LinearInterpolation.linearInterpolation(
+				maxTimeMod, maxOscillationsMod, oscillations.time);
+
 		// FIND DIASTOLIC BLOOD PRESSURE
 		FLAG = false;
-		//int DPPos = 0;
+		// int DPPos = 0;
 		double DP = 0;
 		int i = MAPPos + 1;
-		while (i < maxOscillationsInterp.length && FLAG == false){
-			if ((double) (maxOscillationsInterp[i] / maxOscillationsInterp[MAPPos]) < 0.7){
+		while (i < maxOscillationsInterp.length && FLAG == false) {
+			if ((double) (maxOscillationsInterp[i] / maxOscillationsInterp[MAPPos]) < 0.7) {
 				FLAG = true;
 				DP = curve.pressure[i];
-				//DPPos = i;
+				// DPPos = i;
 			}
 			i = i + 1;
 		}
-		
+
 		// FIND SYSTOLIC BLOOD PRESSURE
 		FLAG = false;
-		//int SPPos = 0;
+		// int SPPos = 0;
 		double SP = 0;
 		i = MAPPos - 1;
-		while (i > 0 && FLAG == false){
-			if ((double) (maxOscillationsInterp[i] / maxOscillationsInterp[MAPPos]) < 0.5){
+		while (i > 0 && FLAG == false) {
+			if ((double) (maxOscillationsInterp[i] / maxOscillationsInterp[MAPPos]) < 0.5) {
 				FLAG = true;
 				SP = curve.pressure[i];
-				//SPPos = i;
+				// SPPos = i;
 			}
 			i = i - 1;
 		}
-				
-		//bloodPressure.setPressureSignal(maxOscillationsMod);
-		//bloodPressure.setTimeSignal(maxTimeMod);
-		
+
+		// bloodPressure.setPressureSignal(maxOscillationsMod);
+		// bloodPressure.setTimeSignal(maxTimeMod);
+
 		// output
 		bloodPressure.setPressureSignal(maxOscillationsInterp);
 		bloodPressure.setTimeSignal(oscillations.time);
 		bloodPressure.setDiastolicBP(DP);
 		bloodPressure.setSystolicBP(SP);
 		bloodPressure.setMeanArterialBP(MAP);
-		
+
 		return bloodPressure;
 	}
 }
