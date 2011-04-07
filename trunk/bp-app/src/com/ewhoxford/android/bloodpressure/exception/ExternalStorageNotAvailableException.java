2b6f65cc9d9680cca5fa381da42aa64dd76a0695
mauro dsantos <mauro.dsantos@gmail.com>
2011-04-07 10:49:38 +0100

    real time working in toshiba folio. correcting small problems. code refactor. data providor is working

---
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/exception/ExternalStorageNotAvailableException.java
@@ -0,0 +1,16 @@
+package com.ewhoxford.android.bloodpressure.exception;
+
+import android.util.Log;
+
+public class ExternalStorageNotAvailableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ExternalStorageNotAvailableException(String tag) {
+		Log.i(tag, "External Storage not available, cannot save file.");
+	}
+
+}
