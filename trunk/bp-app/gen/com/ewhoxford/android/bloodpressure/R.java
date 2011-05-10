3e75d2a81b9cead3b93e7403376d8060635278c0
mauro dsantos <mauro.dsantos@gmail.com>
2011-05-10 16:29:51 +0100

    Standard Hindi translation by Sunali Bhatnagar, added!

--- trunk/bp-app/gen/com/ewhoxford/android/bloodpressure/R.java
+++ trunk/bp-app/gen/com/ewhoxford/android/bloodpressure/R.java
@@ -120,6 +120,7 @@ public final class R {
         /** measure list	
 measure list	
 measure list        
+ measure list
 measure list	
 measure list	
          */
@@ -130,6 +131,7 @@ measure list
         /**  measure 
  measure 
  measure 
+ measure  
  measure 
  measure 
          */
@@ -141,6 +143,7 @@ measure list
         /** measure view page 
 measure view page 
 measure view page 
+ measure view page 
 measure view page 
 measure view page 
          */
