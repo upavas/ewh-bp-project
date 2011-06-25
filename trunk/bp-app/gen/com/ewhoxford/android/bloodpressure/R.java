021f3412fc50c5f283e2f627c716a9648598fa48
mauro dsantos <mauro.dsantos@gmail.com>
2011-06-25 21:42:12 +0100

    building pictograms

--- trunk/bp-app/gen/com/ewhoxford/android/bloodpressure/R.java
+++ trunk/bp-app/gen/com/ewhoxford/android/bloodpressure/R.java
@@ -75,12 +75,13 @@ public final class R {
     }
     public static final class layout {
         public static final int help=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int measure=0x7f030002;
-        public static final int measure_list=0x7f030003;
-        public static final int measure_list_header_row=0x7f030004;
-        public static final int measure_list_row=0x7f030005;
-        public static final int measure_view=0x7f030006;
+        public static final int help2=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int measure=0x7f030003;
+        public static final int measure_list=0x7f030004;
+        public static final int measure_list_header_row=0x7f030005;
+        public static final int measure_list_row=0x7f030006;
+        public static final int measure_view=0x7f030007;
     }
     public static final class raw {
         public static final int bp=0x7f040000;
