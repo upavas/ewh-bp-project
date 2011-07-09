c97970fca2c1331c8f0573ac15d8fb4752cde0bf
mauro dsantos <mauro.dsantos@gmail.com>
2011-07-09 23:03:02 +0100

    added alternative color to bptable

--- trunk/bp-app/src/com/ewhoxford/android/bloodpressure/MeasureActivity.java
+++ trunk/bp-app/src/com/ewhoxford/android/bloodpressure/MeasureActivity.java
@@ -35,6 +35,7 @@ import com.ewhoxford.android.bloodpressure.database.BloodPressureMeasureTable.BP
 import com.ewhoxford.android.bloodpressure.exception.BadMeasureException;
 import com.ewhoxford.android.bloodpressure.model.BloodPressureValue;
 import com.ewhoxford.android.bloodpressure.pressureInputDevice.SampleDynamicXYDatasource;
+import com.ewhoxford.android.bloodpressure.pressureInputDevice.TestDatasource;
 import com.ewhoxford.android.bloodpressure.signalProcessing.SignalProcessing;
 import com.ewhoxford.android.bloodpressure.signalProcessing.TimeSeriesMod;
 import com.ewhoxford.android.bloodpressure.utils.FileManager;
@@ -56,7 +57,7 @@ public class MeasureActivity extends Activity {
 	// Observer object that is notified by pressure data stream observable file
 	private MyPlotUpdater plotUpdater;
 	// Observable object that notifies observer that new values were acquired.
-	private SampleDynamicXYDatasource data;
+	private TestDatasource data;
 	// pressure time series shown in the real time chart
 	private SimpleXYSeries bpMeasureSeries = null;
 	// array with time points
@@ -156,7 +157,7 @@ public class MeasureActivity extends Activity {
 
 			if (maxPressureReached) {
 				// if max pressure reached, check if measurement is now over
-				if (pressureValue < 20) {
+				if (pressureValue < 25) {
 					// o.deleteObservers();
 					data.setActive(false);
 					// measurement is over, we are prepared to determine blood
@@ -313,7 +314,7 @@ public class MeasureActivity extends Activity {
 		// initialize our XYPlot reference and real time update code:
 
 		// getInstance and position datasets:
-		data = new SampleDynamicXYDatasource();
+		data = new TestDatasource(this);
 		// SampleDynamicSeries signalSeries = new SampleDynamicSeries(data, 0,
 		// "Blood Pressure");
 
@@ -454,11 +455,8 @@ public class MeasureActivity extends Activity {
 	 */
 	private void addNewMeasureAndFile(String savedFileName,
 			BloodPressureValue bloodPressureValue, String note, long time) {
-
 		ContentResolver cr = getContentResolver();
-
 		ContentValues values = new ContentValues();
-
 		values.put(BPMeasure.CREATED_DATE, time);
 		values.put(BPMeasure.MODIFIED_DATE, time);
 		values.put(BPMeasure.DP, bloodPressureValue.getDiastolicBP());
@@ -473,11 +471,8 @@ public class MeasureActivity extends Activity {
 			} else {
 				values.put(BPMeasure.MEASUREMENT_FILE_EXIST, true);
 				values.put(BPMeasure.MEASUREMENT_FILE, savedFileName);
-
 			}
-
 		}
-
 		cr.insert(BPMeasure.CONTENT_URI, values);
 	}
 
